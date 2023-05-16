b29e2b1c4d9848508fd8741458b3a3d55b04a6e9
naro_Kim <gracias9022@gmail.com>
2023-05-16 11:58:53 +0900

    [init] : nextjs setup, updated config file

---
+++ src/app/landing/_app.tsx
@@ -0,0 +1,11 @@
+import "@/styles/globals.css";
+
+export default function App() {
+  return (
+    <>
+      <title></title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,5 @@
+export default function Login() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8"></div>
+  );
+}
