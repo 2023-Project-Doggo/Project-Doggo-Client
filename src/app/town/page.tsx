9c8317eddbcf68f8f2f372ca41ffbdb71969b530
naro_Kim <gracias9022@gmail.com>
2023-11-22 12:14:48 +0900

    style: fog 및 스케일 조정

--- src/app/town/page.tsx
+++ src/app/town/page.tsx
@@ -1,32 +1,28 @@
 "use client";
-import {KeyboardControls, KeyboardControlsEntry} from "@react-three/drei";
-import {Suspense, useEffect, useMemo} from "react";
-import {Canvas} from "@react-three/fiber";
-import {Physics} from "@react-three/rapier";
+import { KeyboardControls, KeyboardControlsEntry } from "@react-three/drei";
+import { Suspense, useMemo } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Physics } from "@react-three/rapier";
 import { Controls } from "@/app/town/components/CharacterController";
 import Field from "@/app/town/components/Field";
 
 const Town = () => {
   const map = useMemo<KeyboardControlsEntry<string>[]>(
     () => [
-      {name: Controls.forward, keys: ["ArrowUp", "KeyW"]},
-      {name: Controls.back, keys: ["ArrowDown", "KeyS"]},
-      {name: Controls.left, keys: ["ArrowLeft", "KeyA"]},
-      {name: Controls.right, keys: ["ArrowRight", "KeyD"]},
-      {name: Controls.jump, keys: ["Space"]},
+      { name: Controls.forward, keys: ["ArrowUp", "KeyW"] },
+      { name: Controls.back, keys: ["ArrowDown", "KeyS"] },
+      { name: Controls.left, keys: ["ArrowLeft", "KeyA"] },
+      { name: Controls.right, keys: ["ArrowRight", "KeyD"] },
+      { name: Controls.jump, keys: ["Space"] },
     ],
     []
   );
 
   return (
     <KeyboardControls map={map}>
-      <Canvas
-        camera={{position: [0, 0.5, 2.5], fov: 50}}
-        shadows
-        gl={{preserveDrawingBuffer: true}}
-      >
+      <Canvas shadows gl={{ preserveDrawingBuffer: true }}>
         <color attach={"background"} args={["#e6f8ff"]} />
-        <fog attach='fog' color='#d9f9ff' near={1} far={40} />
+        <fog attach="fog" color="#d9f9ff" near={1} far={400} />
         <Suspense>
           <Physics>
             <Field />
--- src/app/world/characters/Dog.tsx
+++ src/app/world/characters/Dog.tsx
@@ -1,9 +1,10 @@
 "use client";
 import * as THREE from "three";
-import React, {useRef} from "react";
-import {useAnimations, useGLTF} from "@react-three/drei";
-import {GLTF} from "three-stdlib";
-import {useCharacterContext} from "../contexts/CharacterCustomizationContext";
+import React, { useEffect, useRef } from "react";
+import { useAnimations, useGLTF } from "@react-three/drei";
+import { GLTF } from "three-stdlib";
+import { useCharacterContext } from "../contexts/CharacterCustomizationContext";
+import { useCharacterAnimations } from "../contexts/CharacterAnimations";
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -20,17 +21,22 @@ type GLTFActions = Record<ActionName, THREE.AnimationAction>;
 
 export function Dog(props: JSX.IntrinsicElements["group"]) {
   const group = useRef<THREE.Group>(null!);
-  const {nodes, materials, animations} = useGLTF("/assets/Dog.glb") as GLTFResult;
-  const {actions} = useAnimations(animations, group);
-  const {headColor, mouthColor, eyesColor, bodyColor, legsColor, toeColor} =
+  const { nodes, materials, animations } = useGLTF(
+    "/assets/Dog.glb"
+  ) as GLTFResult;
+  const { actions } = useAnimations(animations, group);
+  const characterState = useCharacterAnimations();
+  const { headColor, mouthColor, eyesColor, bodyColor, legsColor, toeColor } =
     useCharacterContext() as any;
+
+  useEffect(() => {}, []);
 
   return (
     <group ref={group} {...props} dispose={null}>
-      <group name='Scene'>
-        <group name='Rig' scale={0.4}>
+      <group name="Scene">
+        <group name="Rig">
           <skinnedMesh
-            name='Mesh'
+            name="Mesh"
             geometry={nodes.Mesh.geometry}
             material={materials.M_Dog}
             skeleton={nodes.Mesh.skeleton}
